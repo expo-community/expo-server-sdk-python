f3fdd1fa83beef826f37d9c75501ee7826b8fd6d
Charlie Cheever <ccheever@gmail.com>
2016-02-10 16:32:22 -0800

    Mostly complete; just needs some debugging

---
+++ exponent_server_sdk/__init__.py
@@ -0,0 +1,38 @@
+__version__ = '0.0.1'
+
+import json
+import urllib
+
+import requests
+
+class Push(object):
+    """Push Notifications"""
+
+    @classmethod
+    def is_exponent_push_token(cls, token):
+        """Returns `True` if the token is an Exponent push token"""
+
+        return (type(token) is str) and token.startswith('ExponentPushToken')
+
+class Client(object):
+    """Client stuff"""
+
+    def publish(self, exponentPushToken=None, message="", data={}):
+        """Sends a push notification with the given options and data"""
+
+        response = requests.post("https://exp.host/--/api/notify/" + urllib.quote_plus(json.dumps({
+            exponentPushToken: exponentPushToken,
+            message: message,
+        })), data=data, headers={
+            'Content-Type': 'application/json',
+        })
+
+        return response
+        if response.status_code == 400:
+            return response
+            raise InvalidPushTokenError(exponentPushToken)
+
+class InvalidPushTokenError(Exception):
+    """Raised when a push token is not a valid ExponentPushToken"""
+
+    pass
